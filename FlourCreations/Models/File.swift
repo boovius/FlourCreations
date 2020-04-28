c63ba4416f99efaca6ae4c56a2444e6163eae288
Joshua Book <joshua.book@gmail.com>
2020-04-28 16:10:15 -0400

    Rename to me.

---
+++ FlourCreations/Models/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  FlourCreations
+//
+//  Created by Joshua Book on 4/28/20.
+//  Copyright © 2020 PagerXYZ. All rights reserved.
+//
+
+import Foundation
+
+struct Creation {
+  var name: String
+  var summary: String?
+}
--- FlourCreationsTests/FlourTests.swift
+++ FlourCreationsTests/FlourTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import DM-iOS-Template
+@testable import FlourCreations
 
-class DM-iOS-TemplateTests: XCTestCase {
+class FlourCreationsTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
